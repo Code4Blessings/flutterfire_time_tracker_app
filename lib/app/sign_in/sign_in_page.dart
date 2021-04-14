cf44c88266852917b91d9289ad589aa716082a23
Code4Blessings <robincw35@gmail.com>
2021-04-14 10:06:49 -0400

    Created reusable button widgets

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterfire_time_tracker_app/common_widgets/custom_raised_button.dart';
 
 class SignInPage extends StatelessWidget {
   @override
@@ -8,17 +9,41 @@ class SignInPage extends StatelessWidget {
         title: Text('Time Tracker'),
         elevation: 7.0,
       ),
-      body: Container(
-        color: Colors.yellow,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              color: Colors.orange,
-              child: SizedBox(height: 100),
+      body: _buildContent(),
+      backgroundColor: Colors.grey[200],
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            'Sign In',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
             ),
-          ],
-        ),
+          ),
+          SizedBox(height: 8.0),
+          SizedBox(height: 10),
+          CustomRaisedButton(
+            child: Text(
+              'Sign in with Google',
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize: 15.0,
+              ),
+            ),
+            color: Colors.white,
+            borderRadius: 4.0,
+            onPressed: () {},
+          )
+        ],
       ),
     );
   }
---
+++ lib/common_widgets/custom_raised_button.dart
@@ -0,0 +1,26 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class CustomRaisedButton extends StatelessWidget {
+  final Widget child;
+  final Color color;
+  final double borderRadius;
+  final VoidCallback onPressed;
+
+  const CustomRaisedButton(
+      {this.color, this.borderRadius, this.onPressed, this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      child: child,
+      color: color,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+        Radius.circular(borderRadius),
+      )),
+      onPressed: onPressed,
+    );
+  }
+}
