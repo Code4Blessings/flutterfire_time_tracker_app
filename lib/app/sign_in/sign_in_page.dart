4be9e76521e4d56b3eea14c26173eaa0f8428ed6
Code4Blessings <robincw35@gmail.com>
2021-04-14 08:43:22 -0400

    Set up Layout for Sign In page

---
+++ lib/app/sign_in/sign_in_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Time Tracker'),
+        elevation: 7.0,
+      ),
+      body: Container(
+        color: Colors.yellow,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              color: Colors.orange,
+              child: SizedBox(height: 100),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
